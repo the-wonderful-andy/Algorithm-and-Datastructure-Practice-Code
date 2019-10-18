4e0b8cb9f19926dea8f1a89a3bebb51f4ff1af6e
Andy <1104188260@qq.com>
2019-10-18 23:18:04 +0900

    Create MatrixChain_BottomUp.h

---
+++ C_Algorithms/MatrixChain_BottomUp.h
@@ -0,0 +1,71 @@
+//MatrixChain_BottomUp.h
+// Created by hehao on 2019/10/13.
+//
+
+#ifndef CALGORITHMS_MATRIXCHAIN_BOTTOMUP_H
+#define CALGORITHMS_MATRIXCHAIN_BOTTOMUP_H
+#include<limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+int OptMatrixChain(int* dim, int n, int **solution);
+int print_Optimal_Parens(int i, int j, int **solution);
+
+int OptMatrixChain(int* dim, int n, int **solution){
+    int m[n][n];
+    for(int i=0; i<n; i++)
+        m[i][i]=0;
+
+    for(int i=n-2; i>=0; i--){// i+1 is # of the starting element of a sequence of parenthesized matrix
+        for(int j=i+1; j<n; j++){// j+1 is # of the last element of a sequence of parenthesized matrix
+            int minCost=INT_MAX;
+            int solutionBuff;
+
+            for(int k=i; k<j; k++){
+                int thiscost=m[i][k]+m[k+1][j]+dim[i]*dim[k+1]*dim[j+1];
+                if(minCost>thiscost){
+                    minCost=thiscost;
+                    solutionBuff=k;
+                }
+
+                m[i][j]=minCost;
+                solution[i][j]=solutionBuff;
+                // printf("%d\n", m[i][j]);
+            }
+
+        }
+    }
+    return m[0][n-1];
+}
+
+int print_Optimal_Parens(int i, int j, int  **solution){
+    if(i==j){
+        printf("A%d", i);
+        return 0;
+    }
+
+    printf("(");
+
+    print_Optimal_Parens(i, solution[i][j], solution);
+    print_Optimal_Parens(solution[i][j]+1, j, solution);
+
+    printf(")");
+
+    return 0;
+}
+
+
+
+int main() {
+    int dim[]={1, 3, 1, 3, 6, 13, 2, 2};
+    int** solution=(int**)malloc(7*sizeof(int*));
+    for(int i=0; i<7; i++){
+        solution[i]=(int*)malloc(7*sizeof(int));
+    }
+
+    int c=OptMatrixChain(dim, 7, solution);
+    printf("the minimum of scalar multiplications: %d\n", c);
+    printf("Optimal parenthesizing: ");
+    print_Optimal_Parens(0, 6, solution);
+    return 0;
+}
+#endif //CALGORITHMS_MATRIXCHAIN_BOTTOMUP_H

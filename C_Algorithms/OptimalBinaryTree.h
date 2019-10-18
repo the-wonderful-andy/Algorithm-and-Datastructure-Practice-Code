4c30d4ed3184e6123947cef6e0b29bf0f458971b
Andy <1104188260@qq.com>
2019-10-18 23:18:12 +0900

    Create OptimalBinaryTree.h

---
+++ C_Algorithms/OptimalBinaryTree.h
@@ -0,0 +1,12 @@
+//OptimalBinaryTree.h
+// Created by hehao on 2019/10/13.
+//
+
+#ifndef CALGORITHMS_OPTIMALBINARYTREE_H
+#define CALGORITHMS_OPTIMALBINARYTREE_H
+int optimalBT(int* p, int* q, int n, int** solution);
+
+int optimalBT(int* p, int* q, int n, int** solution){
+
+}
+#endif //CALGORITHMS_OPTIMALBINARYTREE_H

dd38ac91844b7471871c51281fd1d457a9cddc5d
Andy <1104188260@qq.com>
2019-10-18 23:18:00 +0900

    Update insertSort.h

--- C_Algorithms/insertSort.h
+++ C_Algorithms/insertSort.h
@@ -12,7 +12,7 @@ int insertSort(int* targetArry, int size){
     int j;
     for(int i=1;i<size;i++){
         buffer=targetArry[i];
-        for(j=i-1; j>=0&&targetArry[j]>buffer;j--){
+        for(j=i-1; j>=0&&targetArry[j]<buffer;j--){
             targetArry[j+1]=targetArry[j];
         }
         targetArry[j+1]=buffer;
